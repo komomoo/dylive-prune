ed3ee3cb0ee94d65d8fed67e43ea01e943a8712b
komo <ko.momo@qq.com>
2023-09-19 19:47:29 +0800

    release: 0.6

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         抖音直播精简2023
 // @namespace    https://github.com/komomoo/dylive-prune
-// @version      0.5
+// @version      0.6
 // @description  移除相关直播、移除礼物栏、移除用户进入直播间消息、移除粉丝牌&礼物弹幕、弹幕透明度改为70%、移除点赞动画、进入直播间时自动屏蔽礼物特效。源代码https://github.com/komomoo/dylive-prune，欢迎pr👏
 // @author       komo
 // @match        https://live.douyin.com/*
