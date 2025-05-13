384f959a46d3d81df3ba620318fcfea000e8c103
komo <ko.momo@qq.com>
2025-05-13 21:10:55 +0800

    feat: 入场通知

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         抖音直播精简
 // @namespace    https://github.com/komomoo/dylive-prune
-// @version      0.29
+// @version      0.30
 // @description  移除相关直播、移除礼物栏、移除用户进入直播间消息、移除粉丝牌&礼物弹幕、移除点赞动画、自动屏蔽礼物特效、移除顶栏客户端&快捷访问按钮、自动网页全屏、自动切原画画质。源代码https://github.com/komomoo/dylive-prune，欢迎pr👏
 // @author       komo
 // @match        https://live.douyin.com/*
@@ -24,6 +24,7 @@
   /* 弹幕区用户进入直播间消息 */
   .webcast-chatroom___bottom-message{display:none !important;}
   .Mv2estbj{display:none !important;}
+  .Y0sC3Fms{display:none !important;}
   /* 粉丝牌&礼物弹幕 */
   .ZUGMpBcb{display:none !important;}
   /* 点赞动画 */
